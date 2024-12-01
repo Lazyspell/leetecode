b601743579033fbafd9de5153e81c2345bd724d1
lazyspell <jelam2975@gmail.com>
2024-12-01 11:57:03 -0700

    solved leetcode 27 remove element - python

--- python/main.py
+++ python/main.py
@@ -10,6 +10,17 @@ def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> list[int]:
     return nums1
 
 
+def remove_element(nums: list[int], val: int) -> int:
+    """remove element from given list"""
+    counter = 0
+    for value in nums:
+        if value != val:
+            nums[counter] = value
+            counter += 1
+
+    return counter
+
+
 def main():
     """Main function where all the functions will go through"""
     print("This is the main function.")
@@ -18,6 +29,11 @@ def main():
     merge_list1 = [1, 2, 3, 0, 0, 0]
     merge_list2 = [2, 5, 6]
     merge_list1 = merge(merge_list1, 3, merge_list2, 3)
+    print(merge_list1)
+    print("___________________________________________________________________")
+    print("Removed Element")
+    remove_list = [3, 1, 2, 4, 3, 5, 8]
+    merge_list1 = remove_element(remove_list, 3)
     print(merge_list1)
     print("___________________________________________________________________")
 
